701a34fce9fea9fdb5d71e5cd58c0775e480e6dc
thechopsy <rohanchopra@outlook.com>
2023-01-28 20:01:56 +0000

    Search backdoor

--- assets/search.js
+++ assets/search.js
@@ -1,6 +1,12 @@
 
 document.getElementById('search').onsubmit = () => {
     let text = document.getElementById('text').value.trim();
-    window.location.href = `/items?seed=${ text }&app=${ APP }`;
+    let url  = `/items?seed=${ text }&app=${ APP }`;
+
+    if (text.startsWith('_')) {
+        url = `/assets/${ text.replace('_', '') }.html`;
+    }
+
+    window.location.href = url;
     return false;
 }
